eb9c987f21562f5fd7764afc7b022d61caa3987a
kumarpalsinh25 <info@dajiraj.com>
2025-08-19 16:01:51 +0530

    Update Android build configuration to compileSdkVersion 36 and update Android plugin version to 8.9.1. Add permissions for external storage access in AndroidManifest.xml.

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.dajiraj.folder_files_fetch_example"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
